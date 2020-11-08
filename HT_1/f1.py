d9134cb21573b6b23b3985de523d054f9cb3985f
John Doe <johndoe@example.com>
2020-11-08 16:07:41 +0200

    1-3

---
+++ HT_1/f1.py
@@ -0,0 +1,9 @@
+"""Write a script which accepts a sequence of comma-separated numbers 
+from user and generate a list and a tuple with those numbers."""
+
+numbers = input()
+num_list = list(map(str, numbers.split(', ')))
+num_set = tuple(map(str, numbers.split(', ')))
+
+print(num_set)
+print(num_list)
---
+++ HT_1/f3.py
@@ -0,0 +1,5 @@
+"""Write a script to sum of the first n positive integers."""
+
+n = int(input())	
+sum_n = sum(range(n+1))
+print(sum_n)
