64fc655a0f4b002806af78002fc4d05645074df6
John Doe <johndoe@example.com>
2020-11-08 16:30:05 +0200

    done 6

---
+++ HT_1/f6.py
@@ -0,0 +1,9 @@
+"""Write a script to check whether a specified value is contained in a group of values."""
+
+test_data = [1, 5, 8, 3]
+val = int(input())
+
+if(val in test_data):
+	print(True)
+else:
+	print(False)
