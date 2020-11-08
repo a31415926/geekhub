85a2db4c9757ffb9b226ccaf2ab5c43b5274e69d
John Doe <johndoe@example.com>
2020-11-08 16:10:38 +0200

    1-3

--- HT_1/f1.py
+++ HT_1/f1.py
@@ -3,7 +3,7 @@ from user and generate a list and a tuple with those numbers."""
 
 numbers = input()
 num_list = list(map(str, numbers.split(', ')))
-num_set = tuple(map(str, numbers.split(', ')))
+num_tuple = tuple(map(str, numbers.split(', ')))
 
 print(num_set)
-print(num_list)
+print(num_tuple)
